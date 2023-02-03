d2e0c530b9e876b558801b2c3fdff6f30a775c4e
plotsklapps <plotsklapps@gmail.com>
2023-02-03 14:27:31 +0100

    - Updated FlexColorScheme to 7.0.0-dev; - Updated everything to M3 look.

--- lib/theme/flextheme.dart
+++ lib/theme/flextheme.dart
@@ -1,13 +1,15 @@
 import 'package:boksklapps/all_imports.dart';
 
-/// Light Theme from FlexColorScheme
+/// FlexTheme for LightMode
 ThemeData themeLight = FlexThemeData.light(
   scheme: FlexScheme.outerSpace,
   surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
   blendLevel: 9,
+  tabBarStyle: null,
   subThemesData: const FlexSubThemesData(
     blendOnLevel: 10,
     blendOnColors: false,
+    useM2StyleDividerInM3: true,
   ),
   visualDensity: FlexColorScheme.comfortablePlatformDensity,
   useMaterial3: true,
@@ -15,13 +17,15 @@ ThemeData themeLight = FlexThemeData.light(
   fontFamily: GoogleFonts.bebasNeue().fontFamily,
 );
 
-/// Dark Theme from FlexColorScheme
+/// FlexTheme for DarkMode
 ThemeData themeDark = FlexThemeData.dark(
   scheme: FlexScheme.outerSpace,
   surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
   blendLevel: 15,
+  tabBarStyle: null,
   subThemesData: const FlexSubThemesData(
     blendOnLevel: 20,
+    useM2StyleDividerInM3: true,
   ),
   visualDensity: FlexColorScheme.comfortablePlatformDensity,
   useMaterial3: true,
