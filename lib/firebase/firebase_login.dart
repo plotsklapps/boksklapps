13a4daeb6c6bab97c85df81d258e759f0c60e439
plotsklapps <plotsklapps@gmail.com>
2023-02-17 16:53:04 +0000

    - Added all FirebaseExceptions to register method; - Added snackbars and Logger().

--- lib/firebase/firebase_login.dart
+++ lib/firebase/firebase_login.dart
@@ -1,6 +1,6 @@
 import 'package:boksklapps/all_imports.dart';
 
-/// Extracted method to login to Firebase with all exception handling
+/// Method to login to Firebase with all exception handling
 Future<void> loginToFirebase(
   BuildContext context,
   WidgetRef ref,
@@ -8,11 +8,12 @@ Future<void> loginToFirebase(
   String password,
 ) async {
   try {
-    await FirebaseAuth.instance
+    await ref
+        .watch(firebaseProvider)
         .signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    )
+          email: email,
+          password: password,
+        )
         .then((_) async {
       Logger().i('Logging in with email: $email');
       await Navigator.pushReplacementNamed(
@@ -36,186 +37,276 @@ Future<void> loginToFirebase(
       );
     } else if (error.code == 'app-not-authorized') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The app is not authorized to use Firebase '
             'Authentication',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'credential-already-in-use') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The account corresponding to the emailaddress '
             'already exists with different sign-in credentials',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'email-already-in-use') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The email address is already in use by another account',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'internal-error') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'An internal error has occurred, please try again later',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'invalid-argument') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'An invalid argument was provided, please try again '
             'later',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'invalid-credential') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The credential data is malformed or has expired',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'invalid-email') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The emailaddress is invalid or badly formatted',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'invalid-verification-code') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The verification code is invalid or has expired',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'invalid-verification-id') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The verification ID is invalid or has expired',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'network-request-failed') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'A network error (such as timeout, interrupted connection '
             'or unreachable host) has occurred',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'no-such-provider') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'No such provider exists',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'operation-not-allowed') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The given sign-in provider is disabled for this Firebase '
             'project',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'provider-already-linked') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'User is already linked to the provider',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'quota-exceeded') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The quota for this operation has been exceeded',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'requires-recent-login') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'This operation is sensitive and requires recent '
             'authentication. Log in again before retrying '
             'this request',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'too-many-requests') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'We have blocked all requests from this device due to '
             'unusual activity. Try again later',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'user-disabled') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The user account has been disabled by an administrator',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'user-mismatch') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The supplied credentials do not correspond to the '
             'previously signed in user',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'user-not-found') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'There is no user corresponding to the given email',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'user-token-expired') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             "The user's credential is no longer valid. "
             'The user must sign in again',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'weak-password') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(
+        SnackBar(
+          content: const Text(
             'The password must be 6 characters long or more',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
           ),
         ),
       );
     } else if (error.code == 'wrong-password') {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Wrong password provided for that user'),
+        SnackBar(
+          content: const Text(
+            'Wrong password provided for that user',
+          ),
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: () {},
+          ),
         ),
       );
     } else {
--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -11,128 +11,136 @@ class DrawerWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Drawer(
       child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 220,
-            child: DrawerHeader(
-              child: Column(
-                children: <Row>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Animate>[
-                      Animate(
-                        effects: <Effect<dynamic>>[
-                          ScaleEffect(
-                            delay: 0.ms,
-                            duration: 800.ms,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Column>[
+          Column(
+            children: <SizedBox>[
+              SizedBox(
+                height: 220,
+                child: DrawerHeader(
+                  child: Column(
+                    children: <Row>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Animate>[
+                          Animate(
+                            effects: <Effect<dynamic>>[
+                              ScaleEffect(
+                                delay: 0.ms,
+                                duration: 800.ms,
+                              ),
+                            ],
+                            child: const Text(
+                              StringUtils.kBoks,
+                              style: TextStyleUtils.kLogo1,
+                            ),
+                          ),
+                          Animate(
+                            effects: <Effect<dynamic>>[
+                              ScaleEffect(
+                                delay: 800.ms,
+                                duration: 800.ms,
+                              ),
+                            ],
+                            child: const Text(
+                              StringUtils.kKlapps,
+                              style: TextStyleUtils.kLogo2,
+                            ),
                           ),
                         ],
-                        child: const Text(
-                          StringUtils.kBoks,
-                          style: TextStyleUtils.kLogo1,
-                        ),
                       ),
-                      Animate(
-                        effects: <Effect<dynamic>>[
-                          ScaleEffect(
-                            delay: 800.ms,
-                            duration: 800.ms,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Animate>[
+                          Animate(
+                            effects: <Effect<dynamic>>[
+                              FadeEffect(
+                                delay: 1600.ms,
+                                duration: 800.ms,
+                              ),
+                            ],
+                            child: const Text(
+                              StringUtils.kSparringPartner,
+                              style: TextStyleUtils.kHeadline2,
+                            ),
                           ),
                         ],
-                        child: const Text(
-                          StringUtils.kKlapps,
-                          style: TextStyleUtils.kLogo2,
-                        ),
                       ),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Animate>[
-                      Animate(
-                        effects: <Effect<dynamic>>[
-                          FadeEffect(
-                            delay: 1600.ms,
-                            duration: 800.ms,
-                          ),
-                        ],
-                        child: const Text(
-                          StringUtils.kSparringPartner,
-                          style: TextStyleUtils.kHeadline2,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-          const Spacer(),
-          ListTile(
-            title: const Text(
-              'Account',
-              style: TextStyleUtils.kBodyText,
-            ),
-            subtitle: const Text('Change credential information'),
-            trailing: IconUtils.kAccount,
-            onTap: () async {
-              /// Navigate to account screen
-              await Navigator.pushNamed(
-                context,
-                '/account_screen',
-              );
-            },
-          ),
-          ListTile(
-            title: const Text(
-              'Settings',
-              style: TextStyleUtils.kBodyText,
-            ),
-            subtitle: const Text('Change app look & feel'),
-            trailing: IconUtils.kSettings,
-            onTap: () async {
-              /// Navigate to settings screen
-              await Navigator.pushNamed(
-                context,
-                '/settings_screen',
-              );
-            },
-          ),
-          ListTile(
-            title: const Text(
-              'Logout',
-              style: TextStyleUtils.kBodyText,
-            ),
-            subtitle: const Text('Sign out from the app'),
-            trailing: IconUtils.kLogout,
-            onTap: () async {
-              // Sign out from Firebase
-              ref.read(firebaseProvider).currentUser?.signOut();
+          Column(
+            children: <ListTile>[
+              ListTile(
+                title: const Text(
+                  'Account',
+                  style: TextStyleUtils.kBodyText,
+                ),
+                subtitle: const Text('Change credential information'),
+                trailing: IconUtils.kAccount,
+                onTap: () async {
+                  /// Navigate to account screen
+                  await Navigator.pushNamed(
+                    context,
+                    '/account_screen',
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  'Settings',
+                  style: TextStyleUtils.kBodyText,
+                ),
+                subtitle: const Text('Change app look & feel'),
+                trailing: IconUtils.kSettings,
+                onTap: () async {
+                  /// Navigate to settings screen
+                  await Navigator.pushNamed(
+                    context,
+                    '/settings_screen',
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  'Logout',
+                  style: TextStyleUtils.kBodyText,
+                ),
+                subtitle: const Text('Sign out from the app'),
+                trailing: IconUtils.kLogout,
+                onTap: () async {
+                  // Sign out from Firebase
+                  await ref.read(firebaseProvider).signOut();
 
-              // Clear the currentUserProvider
-              ref.invalidate(currentUserProvider);
+                  // Clear the currentUserProvider
+                  ref.invalidate(currentUserProvider);
 
-              // Clear all screens, show snackbar to user
-              // and return to splash screen
-              if (context.mounted) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: const Text(
-                      'You have been signed out...',
-                    ),
-                    action: SnackBarAction(
-                      label: 'OK',
-                      onPressed: () {},
-                    ),
-                  ),
-                );
-                await Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  '/',
-                  (Route<dynamic> route) => false,
-                );
-              }
-            },
+                  // Clear all screens, show snackbar to user
+                  // and return to splash screen
+                  if (context.mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text(
+                          'You have been signed out...',
+                        ),
+                        action: SnackBarAction(
+                          label: 'OK',
+                          onPressed: () {},
+                        ),
+                      ),
+                    );
+                    await Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      '/',
+                      (Route<dynamic> route) => false,
+                    );
+                  }
+                },
+              ),
+            ],
           ),
         ],
       ),
