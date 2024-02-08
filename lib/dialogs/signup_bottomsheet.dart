792580a43dc4316e520938a8fc182c7d77fa0537
plotsklapps <plotsklapps@gmail.com>
2024-02-08 11:19:17 +0000

    - Signupbottomsheet has logic removed from UI now.

--- lib/dialogs/signup_bottomsheet.dart
+++ lib/dialogs/signup_bottomsheet.dart
@@ -2,6 +2,8 @@ import 'package:boksklapps/auth_service.dart';
 import 'package:boksklapps/main.dart';
 import 'package:boksklapps/screens/verify_screen.dart';
 import 'package:boksklapps/signals/showspinner_signal.dart';
+import 'package:boksklapps/theme/flexcolors.dart';
+import 'package:boksklapps/theme/flextheme.dart';
 import 'package:boksklapps/theme/text_utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +27,9 @@ class BottomSheetSignupState extends State<BottomSheetSignup> {
 
   final AuthService _authService = AuthService();
 
-  Signal<bool> _isObscured = signal<bool>(true);
+  final GlobalKey<FormState> _signupFormKey = GlobalKey<FormState>();
+
+  Signal<bool> _isObscured = signal<bool>(false);
 
   @override
   void initState() {
@@ -56,11 +60,100 @@ class BottomSheetSignupState extends State<BottomSheetSignup> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text('Create BOKSklapps account', style: TextUtils.fontL),
+            const Text('Create a BOKSklapps account', style: TextUtils.fontL),
             const Divider(thickness: 2),
             const SizedBox(height: 16),
-            const SignupForm(),
-            const SizedBox(height: 16),
+            Form(
+              key: _signupFormKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _emailController,
+                    validator: (String? value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          !value.contains('@') ||
+                          !value.contains('.')) {
+                        return 'Please enter a correct emailaddress.';
+                      } else {
+                        return null;
+                      }
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(label: Text('Email')),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _passwordController,
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty || value.length < 6) {
+                        return 'Password needs to be at least 6 characters.';
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: _isObscured.watch(context),
+                    enableSuggestions: false,
+                    decoration: InputDecoration(
+                      label: const Text('Password'),
+                      suffixIcon: _isObscured.watch(context)
+                          ? GestureDetector(
+                              onTap: () {
+                                _isObscured.value = false;
+                              },
+                              child: const FaIcon(
+                                FontAwesomeIcons.eye,
+                              ),
+                            )
+                          : GestureDetector(
+                              onTap: () {
+                                _isObscured.value = true;
+                              },
+                              child: const FaIcon(
+                                FontAwesomeIcons.eyeSlash,
+                              ),
+                            ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty || value.length < 6) {
+                        return 'Passwords needs to be at least 6 characters.';
+                      } else if (value != _passwordController.text) {
+                        return 'Passwords do not appear to be equal.';
+                      } else {
+                        return null;
+                      }
+                    },
+                    obscureText: _isObscured.watch(context),
+                    enableSuggestions: false,
+                    decoration: InputDecoration(
+                      label: const Text('Confirm Password'),
+                      suffixIcon: _isObscured.watch(context)
+                          ? GestureDetector(
+                              onTap: () {
+                                _isObscured.value = false;
+                              },
+                              child: const FaIcon(
+                                FontAwesomeIcons.eye,
+                              ),
+                            )
+                          : GestureDetector(
+                              onTap: () {
+                                _isObscured.value = true;
+                              },
+                              child: const FaIcon(
+                                FontAwesomeIcons.eyeSlash,
+                              ),
+                            ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
@@ -73,21 +166,7 @@ class BottomSheetSignupState extends State<BottomSheetSignup> {
                 const SizedBox(width: 16),
                 FloatingActionButton(
                   onPressed: () async {
-                    sSpinnerSignup.value = true;
-                    final String email = _emailController.text.trim();
-                    final String password = _passwordController.text.trim();
-                    // Create a new Firebase user.
-                    await _authService.createUserWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                      onError: _handleErrors,
-                      onSuccess: (UserCredential userCredential) async {
-                        await _handleSuccess(
-                          userCredential: userCredential,
-                          email: email,
-                        );
-                      },
-                    );
+                    await _validateAndCreate();
                   },
                   // Watching a computed signal to provide the
                   // corresponding Widget.
@@ -101,13 +180,59 @@ class BottomSheetSignupState extends State<BottomSheetSignup> {
     );
   }
 
+  Future<void> _validateAndCreate() async {
+    if (_signupFormKey.currentState!.validate()) {
+      sSpinnerSignup.value = true;
+      final String email = _emailController.text.trim();
+      final String password = _passwordController.text.trim();
+      // Create a new Firebase user.
+      await _authService.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+        onError: _handleErrors,
+        onSuccess: (UserCredential userCredential) async {
+          await _handleSuccess(
+            userCredential: userCredential,
+            email: email,
+          );
+        },
+      );
+    } else {
+      rootScaffoldMessengerKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text(
+            'Please check the email and password.',
+            style: TextStyle(
+              color: sDarkTheme.value
+                  ? flexSchemeDark.onError
+                  : flexSchemeLight.onError,
+            ),
+          ),
+          showCloseIcon: true,
+          backgroundColor:
+              sDarkTheme.value ? flexSchemeDark.error : flexSchemeLight.error,
+        ),
+      );
+      return;
+    }
+  }
+
   void _handleErrors(String error) {
     Logger().e('Error: $error');
     sSpinnerSignup.value = false;
     rootScaffoldMessengerKey.currentState!.showSnackBar(
       SnackBar(
-        content: Text('Error: $error'),
+        content: Text(
+          'Error: $error',
+          style: TextStyle(
+            color: sDarkTheme.value
+                ? flexSchemeDark.onError
+                : flexSchemeLight.onError,
+          ),
+        ),
         showCloseIcon: true,
+        backgroundColor:
+            sDarkTheme.value ? flexSchemeDark.error : flexSchemeLight.error,
       ),
     );
   }
@@ -161,137 +286,6 @@ class BottomSheetSignupState extends State<BottomSheetSignup> {
           ),
         );
       },
-    );
-  }
-}
-
-class SignupForm extends StatefulWidget {
-  const SignupForm({super.key});
-
-  @override
-  State<SignupForm> createState() {
-    return SignupFormState();
-  }
-}
-
-class SignupFormState extends State<SignupForm> {
-  late TextEditingController _emailController;
-  late TextEditingController _passwordController;
-  late TextEditingController _confirmPasswordController;
-
-  final GlobalKey<FormState> _emailFormKey = GlobalKey<FormState>();
-  final GlobalKey<FormState> _passwordFormKey = GlobalKey<FormState>();
-  final GlobalKey<FormState> _confirmPasswordFormKey = GlobalKey<FormState>();
-
-  Signal<bool> _isObscured = signal<bool>(false);
-
-  @override
-  void initState() {
-    super.initState();
-    _emailController = TextEditingController();
-    _passwordController = TextEditingController();
-    _confirmPasswordController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    _confirmPasswordController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            controller: _emailController,
-            key: _emailFormKey,
-            validator: (String? value) {
-              if (value == null || value.isEmpty || !value.contains('@')) {
-                return 'Please enter a correct emailaddress.';
-              } else {
-                return null;
-              }
-            },
-            autofocus: true,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(label: Text('Email')),
-          ),
-          const SizedBox(height: 8),
-          TextFormField(
-            controller: _passwordController,
-            key: _passwordFormKey,
-            validator: (String? value) {
-              if (value == null || value.isEmpty || value.length < 6) {
-                return 'Password needs to be at least 6 characters.';
-              } else {
-                return null;
-              }
-            },
-            obscureText: _isObscured.watch(context),
-            enableSuggestions: false,
-            decoration: InputDecoration(
-              label: const Text('Password'),
-              suffixIcon: _isObscured.watch(context)
-                  ? GestureDetector(
-                      onTap: () {
-                        _isObscured.value = false;
-                      },
-                      child: const FaIcon(
-                        FontAwesomeIcons.eye,
-                      ),
-                    )
-                  : GestureDetector(
-                      onTap: () {
-                        _isObscured.value = true;
-                      },
-                      child: const FaIcon(
-                        FontAwesomeIcons.eyeSlash,
-                      ),
-                    ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          TextFormField(
-            controller: _confirmPasswordController,
-            key: _confirmPasswordFormKey,
-            validator: (value) {
-              if (value == null || value.isEmpty || value.length < 6) {
-                return 'Passwords needs to be at least 6 characters.';
-              } else if (value != _passwordController.text) {
-                return 'Passwords do not appear to be equal.';
-              } else {
-                return null;
-              }
-            },
-            obscureText: _isObscured.watch(context),
-            enableSuggestions: false,
-            decoration: InputDecoration(
-              label: const Text('Confirm Password'),
-              suffixIcon: _isObscured.watch(context)
-                  ? GestureDetector(
-                      onTap: () {
-                        _isObscured.value = false;
-                      },
-                      child: const FaIcon(
-                        FontAwesomeIcons.eye,
-                      ),
-                    )
-                  : GestureDetector(
-                      onTap: () {
-                        _isObscured.value = true;
-                      },
-                      child: const FaIcon(
-                        FontAwesomeIcons.eyeSlash,
-                      ),
-                    ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
